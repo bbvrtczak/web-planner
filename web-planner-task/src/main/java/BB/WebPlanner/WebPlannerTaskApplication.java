56af00d3c5b4a004dfea8db8f64d67f3666cb104
Bartosz Bartczak <bartoszbartczak@op.pl>
2024-02-02 19:08:00 +0100

    Created base project for task management

---
+++ web-planner-task/src/main/java/BB/WebPlanner/WebPlannerTaskApplication.java
@@ -0,0 +1,13 @@
+package BB.WebPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPlannerTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPlannerTaskApplication.class, args);
+	}
+
+}
---
+++ web-planner-task/src/test/java/BB/WebPlanner/Member/GuildsAndMembersMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package BB.WebPlanner.Member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildsAndMembersMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
