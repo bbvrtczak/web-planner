c1e6f2dd020e9d526e8e3897c7a721cd6bd14c4a
Bartosz Bartczak <bartoszbartczak@op.pl>
2024-02-07 14:06:58 +0100

    Added CORS configuration

--- web-planner-react/src/App.tsx
+++ web-planner-react/src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8081/api/tasks')
+       .then((res) => res.json())
+       .then((data) => {
+          console.log(data);
+          setPosts(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }, []);
 
   return (
     <>
